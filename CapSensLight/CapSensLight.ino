87605ccfbc567516c33091effcadc8c907bb8f2f
Mathias Sass Michno <mmichn13@student.aau.dk>
2015-11-14 00:50:50 +0100

    Add fade on hold and tweek to work on 12v wall power

--- CapSensLight/CapSensLight.ino
+++ CapSensLight/CapSensLight.ino
@@ -1,33 +1,70 @@
 #include <CapacitiveSensor.h>
-#define TRIGGER_VAL 2000
+#include <Timer.h>
+#define TRIGGER_VAL 220
 #define RECV_PIN 2
 #define SEND_PIN 4
-#define OUTP_PIN 13
+#define LED_PIN 9
+#define LONG_PRESS_MILLIS 200
 
 CapacitiveSensor   capSens = CapacitiveSensor(SEND_PIN, RECV_PIN);        // 1M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil
 bool isPressed = false;
 bool state = false;
+int brightness = 0;    // how bright the LED is
+int fadeAmount = 1;    // how many points to fade the LED by
+unsigned long pressedDelta = 0;
 
-void setup()                    
-{
-  pinMode(OUTP_PIN, OUTPUT);
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+#ifdef DEBUG
   Serial.begin(9600);
+#endif
 }
 
-void loop()                    
-{
+void loop() {
   long capSensVal =  capSens.capacitiveSensor(30);
-  if(!isPressed && capSensVal > TRIGGER_VAL){
+  if(!isPressed && capSensVal >= TRIGGER_VAL){
     isPressed = true;
-    switchState();  
-  } else if(capSensVal <= TRIGGER_VAL) {
+    pressedDelta = millis(); 
+  }else if(isPressed && millis() - pressedDelta >= LONG_PRESS_MILLIS) {
+    fade();
+    isPressed = !(capSensVal < TRIGGER_VAL);  
+  }else if(isPressed && capSensVal < TRIGGER_VAL) {
+    switchState();
     isPressed = false;
+    pressedDelta = 0;
   }
-  delay(20);                             // arbitrary delay to limit data to serial port 
 }
 
-void switchState()
-{
-  state = !state;      
-  digitalWrite(OUTP_PIN, state);
+void switchState() {
+  state = !brightness;      
+  digitalWrite(LED_PIN, state);
+  brightness  = state ? 255 : 0;
+  fadeAmount *= (state && fadeAmount < 0) ? -1 : 1;
+#ifdef DEBUG
+  Serial.print("SWITCH\t");
+  Serial.println(state ? "on" : "off");
+#endif
+  delay(400);
+}
+
+void fade(){
+  analogWrite(LED_PIN, brightness);
+  brightness  = brightness + fadeAmount;
+  fadeAmount *= (brightness == 0 || brightness == 255) ? -1  : 1;
+#ifdef DEBUG
+  Serial.print("FADE\t");
+  Serial.print(fadeAmount < 0 ? "-" : "+");
+  Serial.print("\t");
+  Serial.print(brightness);
+  Serial.print("\t");
+  for(int i = 0; i < brightness / 10; i++){
+    Serial.print("-");  
+  }
+  Serial.println("");
+#endif
+  if(brightness < 0 || brightness > 255){
+    fadeAmount *= (brightness < 0 ? (fadeAmount < 0 ? -1 : 1) : (fadeAmount > 0 ? -1 : 1));
+    brightness  = brightness < 0 ? 0 : 255;   
+  }
+  delay(20);  
 }
